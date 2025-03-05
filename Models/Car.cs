10f1d1cd8c9be9f70ed4b0ddcb60e00bad0212aa
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2025-03-05 20:16:10 +0200

    Update Car.cs

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,12 +3,12 @@ namespace ASPNetExapp.Models
     public class Car
     {
         public int Id { get; set; }
-        public string? Number { get; set; }
+        public string Number { get; set; } 
         public int Year { get; set; }
-        public string? Brand { get; set; }
-        public string? Color { get; set; }
-        public string? Condition { get; set; }
-        public string? OwnerLastName { get; set; }
-        public string? Address { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Condition { get; set; }
+        public string OwnerLastName { get; set; }
+        public string Address { get; set; }
     }
 }
